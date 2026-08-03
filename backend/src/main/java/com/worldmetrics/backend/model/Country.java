2284114bbc0001ef2a4c3fbb2eb0fbee186eaadd
giannis <giannis85m@gmail.com>
2026-08-03 22:10:19 +0300

    fix: correct @Table annotation in entity classes

--- backend/src/main/java/com/worldmetrics/backend/model/Country.java
+++ backend/src/main/java/com/worldmetrics/backend/model/Country.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "indicators")
+@Table(name = "countries")
 @Getter
 @Setter
 @NoArgsConstructor
--- backend/src/main/java/com/worldmetrics/backend/model/Indicator.java
+++ backend/src/main/java/com/worldmetrics/backend/model/Indicator.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table
+@Table(name = "indicators")
 @Getter
 @Setter
 @NoArgsConstructor
